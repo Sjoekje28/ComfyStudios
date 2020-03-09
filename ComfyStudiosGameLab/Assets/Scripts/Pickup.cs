ffc0c691e37d34da7dda721ce01780ee3098064e
ZipNotRar <61015917+ZipNotRar@users.noreply.github.com>
2020-03-09 11:37:38 +0100

    Inventory_Start

---
+++ ComfyStudiosGameLab/Assets/Scripts/Pickup.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickup : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject itemButton;
+    public GameObject popup;
+    public Button choice1;
+    public Button choice2;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        choice1.GetComponent<Button>();
+
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            popup.SetActive(true);
+            Time.timeScale = 0;
+            choice1.onClick.AddListener(pickup);
+            choice2.onClick.AddListener(discard);
+        }
+    }
+    public void pickup()
+    {
+        for (int i = 0; i < inventory.slots.Length; i++)
+        {
+            if (inventory.slotsFull[i] == false)
+            {
+                inventory.slotsFull[i] = true;
+                Instantiate(itemButton, inventory.slots[i].transform, false);
+                Destroy(gameObject);
+                break;
+            }
+        }
+        popup.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void discard()
+    {
+        Time.timeScale = 1;
+        popup.SetActive(false);
+        Destroy(gameObject);
+    }
+}
