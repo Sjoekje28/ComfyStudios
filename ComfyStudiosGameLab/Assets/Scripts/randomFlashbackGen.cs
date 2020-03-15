5a675bb8e12536045db0334cc088743f4a72ae6f
ZipNotRar <61015917+ZipNotRar@users.noreply.github.com>
2020-03-15 14:05:38 +0100

    Flashbacks update
    Added & updated the Flashbacks option and changing color in case of unable to flashback

---
+++ ComfyStudiosGameLab/Assets/Scripts/randomFlashbackGen.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class randomFlashbackGen : MonoBehaviour
+{
+    int fbCheck;
+    public Button checker;
+    GameObject objRef;
+    public Color c = Color.red;
+    void Start()
+    {
+        checker = GetComponent<Button>();
+        checker.onClick.AddListener(fbChecker);
+    }
+    public void fbChecker()
+    {
+        
+        fbCheck = Random.Range(0, 4);
+        if (fbCheck == 0)
+        {
+
+            Debug.Log("Flashback Happens");
+            //SceneManager.LoadScene("Flashback_Wrench");
+        }
+        else
+        {
+            Debug.Log("Color is changed");
+            objRef = GameObject.Find("wrench_button(Clone)");
+            objRef.GetComponent<Image>().color = c;
+            //objRef = GameObject.Find("knife_button(Clone)");
+            //objRef.GetComponent<Image>().color = c;
+            //objRef = GameObject.Find("Phone_Button(Clone)");
+            //Debug.Log("The gameobject is " + objRef);
+        }
+    }
+}
