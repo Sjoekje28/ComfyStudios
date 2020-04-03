f4b7fe04ab0d328e7e7a9bbf450d42da82478c25
Sjoekje28 <47067156+Sjoekje28@users.noreply.github.com>
2020-04-03 15:26:55 +0200

    Oookeeeey

---
+++ ComfyStudiosGameLab/Assets/Scripts/JournalEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JournalEvent : MonoBehaviour
+{
+    public Button jb;
+    public GameObject jbUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        jb.GetComponent<Button>();
+        jb.onClick.AddListener(jbPopup);
+        jbUI.SetActive(false); 
+    }
+
+    private void jbPopup()
+    {
+        jbUI.SetActive(true);
+    }
+}
