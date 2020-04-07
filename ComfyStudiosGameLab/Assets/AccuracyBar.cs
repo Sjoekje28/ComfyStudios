a32c6831615e88622e755a30e442a8d71f478995
Sjoekje28 <47067156+Sjoekje28@users.noreply.github.com>
2020-04-07 12:52:27 +0200

    quick push

---
+++ ComfyStudiosGameLab/Assets/AccuracyBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AccuracyBar : MonoBehaviour
+{
+    public Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMinAccuracy(int accuracy)
+    {
+        slider.minValue = accuracy;
+        slider.value = accuracy;
+    }
+
+    public void SetAccuracy(int accuracy)
+    {
+        slider.value = accuracy;
+    }
+}
