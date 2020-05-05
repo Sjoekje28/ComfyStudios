1c735a7f926bfe5a734a67092102e6f632b78634
ZipNotRar <61015917+ZipNotRar@users.noreply.github.com>
2020-05-05 10:48:13 +0200

    World Collision & Street names Update
    1. Added the barriers for the world boundary (Task put on hold).
    2. Added the streetnames img for reference in future.

---
+++ ComfyStudiosGameLab/Assets/collisionChecker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionChecker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<BoxCollider2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Player"))
+        {
+            Debug.Log("Hit");
+        }
+    }
+}
