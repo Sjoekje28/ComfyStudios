351f5ccc16f94eb743e2979bc51d534bff4fa0e3
ZipNotRar <61015917+ZipNotRar@users.noreply.github.com>
2020-05-17 19:22:29 +0200

    Updates
    1. Added the Murder Eye button.
    2. Worked on the Carriage item placement in the Murder Scene.
    3. Implemented a PauseMenu System.

---
+++ ComfyStudiosGameLab/Assets/Scripts/PauseTransition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PauseTransition : MonoBehaviour
+{
+    public Button pb;
+    public GameObject pm;
+    public Button rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //pb.GetComponent<Button>();
+        pb.onClick.AddListener(Transition);
+        pm.SetActive(false);
+        rb.onClick.AddListener(resume);
+    }
+
+    void Transition()
+    {
+        pm.SetActive(true);
+        Time.timeScale = 0;
+    }
+    void resume()
+    {
+        pm.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
