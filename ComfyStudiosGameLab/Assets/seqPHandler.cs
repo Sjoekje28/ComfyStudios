ad98b8547eb1fa9613b571e1c8456fb897d8e9ff
ZipNotRar <61015917+ZipNotRar@users.noreply.github.com>
2020-06-09 22:44:28 +0200

    Updates
    1. Added the sliding puzzle systems.
    2. Added the sequence puzzle section.
    3. The sequence puzzle section is ready to be made.

---
+++ ComfyStudiosGameLab/Assets/seqPHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class seqPHandler : MonoBehaviour
+{
+    Inventory Inventory;
+    public GameObject ME;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        ME.GetComponent<Button>().interactable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Inventory.slots[4])
+        {
+            ME.GetComponent<Image>().color = Color.white;
+            ME.GetComponent<Button>().interactable = true;
+            ME.GetComponent<Button>().enabled = true;
+        }
+        ME.GetComponent<Button>().onClick.AddListener(sequencePuzzleInit);
+    }
+    public void sequencePuzzleInit()
+    {
+        Time.timeScale = 0;
+        Debug.Log("Make the sequence puzzle happen now!");
+    }
+}
